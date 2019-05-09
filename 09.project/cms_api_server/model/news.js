95b46cb81b2a2c9fe720ad43bc34c7417f064935
pengleichen <chenpenglei000@yahoo.com>
2019-05-09 11:27:00 +0800

    ignore

--- 09.project/cms_api_server/model/news.js
+++ 09.project/cms_api_server/model/news.js
@@ -1,14 +1,14 @@
 const sequelize = require('../db')
-const News = sequelize.define('news', {
-  title: {
-    id:{ //自增长id,主键,整形
-      type:Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement:true
-    },
-    title: {
-      type: Sequelize.STRING(254)
-    }
-  }
+const Model = sequelize.Model
+class News extends Model { }
 
+News.init({
+  id: { //自增长id,主键,整形
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: Sequelize.STRING(254)
+  }
 })
